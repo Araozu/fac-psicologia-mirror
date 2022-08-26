cc8c1ca1e1236a26c85e9bb22251819cefae44fb
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-08-26 18:23:32 -0500

    Update download file

--- src/components/Form/Components/ItemEvidencia.jsx
+++ src/components/Form/Components/ItemEvidencia.jsx
@@ -9,23 +9,26 @@ export default function ItemEvidencia(props){
     const handleDownload = () => {
         const token = localStorage.getItem("access_token")
 
+
         axios({
             url: download_url, //your url
             method: 'GET',
             headers: {'Authorization': 'Bearer '+token},
             responseType: 'blob', // important
         }).then((response) => {
-            let mimeType = String(response.headers['content-type']);
-            const db = require('mime-db')
-            let extension = db['text/plain']?.extensions[0];
-            console.log(extension)
+            //let mimeType = String(response.headers['content-type']);
+            //const db = require('mime-db')
+            //let extension = db['text/plain']?.extensions[0];
+            //console.log(extension)
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
             link.setAttribute('download', `${denominacion}.pdf`); //or any other extension
             document.body.appendChild(link);
             link.click();
-        });
+		}).catch((error) => {
+			console.log(error);
+		});
     }
 
     return (
