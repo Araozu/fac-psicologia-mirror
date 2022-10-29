29be8a139dfb0cbc8c4b6b7504a103969803248d
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2022-10-29 18:21:24 -0500

    fix pdf-render (remove) VITE CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import {fileURLToPath, URL} from "url";
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
-import shimReactPdf from "vite-plugin-shim-react-pdf";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), shimReactPdf()],
+    plugins: [react()],
     build: {
         sourcemap: false,
     },
